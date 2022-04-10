2fb8b48cc392ac2250b41450ff543834f4bd0498
Trevor Isaacson <trevorisaacson3@gmail.com>
2022-04-10 15:36:54 -0600

    add main file

---
+++ src/cs455/aqi/q1/Main.java
@@ -0,0 +1,19 @@
+package cs455.aqi.q1;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+
+}
