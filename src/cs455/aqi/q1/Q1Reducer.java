4f60a3dd1c0c6f93da7c12b27659e7434ed0874e
Trevor Isaacson <trevorisaacson3@gmail.com>
2022-04-12 10:05:27 -0600

    update imports

--- src/cs455/aqi/q1/Q1Reducer.java
+++ src/cs455/aqi/q1/Q1Reducer.java
@@ -6,11 +6,9 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
 
 /*
     Receives: <Day, AQI Score>
@@ -18,7 +16,6 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
     Finds mean by dividing sum by number of entries for that day
     Returns means for days of week as <Day, mean> 
 */
-
 
 public class Q1Reducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     @Override
