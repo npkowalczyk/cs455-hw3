2341e2df926286dd408e084deade37909e0db7e7
Trevor Isaacson <trevorisaacson3@gmail.com>
2022-04-11 22:39:34 -0600

    reducer

--- src/cs455/aqi/q1/Q1Reducer.java
+++ src/cs455/aqi/q1/Q1Reducer.java
@@ -20,7 +20,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 */
 
 
-public class Q1Reducer extends Reducer<Text, NullWritable, IntWritable, NullWritable> {
+public class Q1Reducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     @Override 
     public void setup(Context context) throws IOException, InterruptedException{
         // creates data object that holds each day (key, value)
@@ -28,12 +28,27 @@ public class Q1Reducer extends Reducer<Text, NullWritable, IntWritable, NullWrit
     }
 
     @Override
-    public void reduce(Text key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException{
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
         // gets 7 keys corresponding to the days of the week
         // checks if data object has certain day of week (key)
             // If it does, add to that and get average
         // basically, check data object and update it
+        try{
+            // sum of all aqi scores
+            int sum = 0;
+            // keeps track of number of items for key
+            int num = 0;
+            for(IntWritable val : values){
+                num += 1;
+                sum += val.get();
+            }
+            int avg = sum / num;
+            context.write(key, new IntWritable(avg));
+            print(key, avg);
 
+        } catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
 
