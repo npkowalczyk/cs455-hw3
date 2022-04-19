609e8716443ad5678dc06cb7986b9cc215b0e262
Trevor Isaacson <trevorisaacson3@gmail.com>
2022-04-19 10:54:16 -0600

    add files

---
+++ src/cs455/aqi/q5/Q5Job.java
@@ -0,0 +1,41 @@
+package cs455.aqi.q5;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import java.io.IOException;
+
+public class Q5Job{
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "q5");
+        // current class
+        job.setJarByClass(Q5Job.class);
+        // Mapper
+        job.setMapperClass(Q5Mapper.class);
+        // Combiner
+        //job.setCombinerClass(IntSumReducer.class);
+        // Reducer
+        job.setReducerClass(Q5Reducer.class);
+        // Outputs from the Mapper
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        // Outputs from the Reducer
+        job.setOutputKeyClass(Text.class);
+        job.setOutputKeyClass(LongWritable.class);
+
+        job.setNumReduceTasks(1); 
+        System.out.println(args[0]);
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }      
+}
