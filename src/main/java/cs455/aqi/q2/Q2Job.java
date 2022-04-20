acdac137ec1408453ef4b4888d3d1c2f55287ee5
Nick Kowalczyk <work.kowalczyk@gmail.com>
2022-04-19 21:20:23 -0600

    fixed q2

--- src/main/java/cs455/aqi/q2/Q2Job.java
+++ src/main/java/cs455/aqi/q2/Q2Job.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -30,12 +31,12 @@ public class Q2Job{
         job.setMapOutputValueClass(IntWritable.class);
         // Outputs from the Reducer
         job.setOutputKeyClass(Text.class);
-        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputKeyClass(DoubleWritable.class);
         // set number of tasks
         job.setNumReduceTasks(1); 
         System.out.println(args[0]);
-        FileInputFormat.addInputPath(job, new Path(args[1]));
-        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }      
 }
