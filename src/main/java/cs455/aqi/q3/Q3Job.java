6afdf6daa89ce2a28c0667199c417064dc4a1073
Trevor Isaacson <trevorisaacson3@gmail.com>
2022-04-19 20:46:02 -0600

    add job

---
+++ src/main/java/cs455/aqi/q3/Q3Job.java
@@ -0,0 +1,39 @@
+package cs455.aqi.q3;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import java.io.IOException;
+
+public class Q3Job{
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "q3");
+        // current class
+        job.setJarByClass(Q3Job.class);
+        // Mapper
+        job.setMapperClass(Q3Mapper.class);
+        // Reducer
+        job.setReducerClass(Q3Reducer.class);
+        // Outputs from the Mapper
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        // Outputs from the Reducer
+        job.setNumReduceTasks(1); 
+        job.setOutputKeyClass(Text.class);
+        job.setOutputKeyClass(LongWritable.class);
+
+        System.out.println(args[0]);
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }      
+}
